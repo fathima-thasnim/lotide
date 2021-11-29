e29035a0e3a5e9fd0cb3b250e710290d7be1639b
fathima-thasnim <thasnimfathima492@gmail.com>
2021-11-29 20:59:52 +0000

    added test file

--- head.js
+++ head.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual')
 
 // TEST CODE
 // assertEqual("Lighthouse Labs", "Bootcamp");
@@ -14,5 +8,7 @@ const head = function(arr1){
   let element = arr1.shift();
   console.log(element);
 }
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+ 
+module.exports = head;
--- takeUntil.js
+++ takeUntil.js
@@ -20,14 +20,16 @@ const assertArraysEqual = function(actualArr,expectedArr){
    const results = [];
 
    for (let item of array) {
+     console.log(item);
     if (callback(item)) {
-      
+      console.log(results)
       return results;
-       
-    } else {
-       
-      results.push(item);
       
+      
+    } else {
+      
+      results.push(item);
+      console.log(results)
     }
   }
    return results;
@@ -36,7 +38,7 @@ const assertArraysEqual = function(actualArr,expectedArr){
 const results1 = takeUntil(data1, x => x < 0);
 assertArraysEqual(results1,[ 1, 2, 5, 7, 2 ]);
 
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-assertArraysEqual(results2,[ 'I\'ve', 'been', 'to', 'Hollywood' ]);
+// const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+// const results2 = takeUntil(data2, x => x === ',');
+// assertArraysEqual(results2,[ 'I\'ve', 'been', 'to', 'Hollywood' ]);
 
