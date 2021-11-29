68be0bc2114a04cba2683c83db44c58beb5e0411
fathima-thasnim <thasnimfathima492@gmail.com>
2021-11-29 22:31:58 +0000

    refactor assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,6 @@
-const eqArrays = function(arr1,arr2){
-  if(arr1.length !== arr2.length) {
-    return "arrays are not same length";
-  }
-  for(let i = 0; i <= arr1.length; i++){
-    if(arr1[i] !== arr2[i]){
-      return false;
-    }
-  }
-return true;
-};
+const eqArrays = require('./eqArrays');
+
+
 const assertArraysEqual = function(actualArr,expectedArr){
   if (eqArrays(actualArr,expectedArr)) {
     console.log(`✅✅✅Assertation Passed: ${actualArr} === ${expectedArr}`);
@@ -16,4 +8,6 @@ const assertArraysEqual = function(actualArr,expectedArr){
     console.log(`🛑🛑🛑Assertation Failed: ${actualArr} !== ${expectedArr}`);
   }
 }
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]))
+
+module.exports = assertArraysEqual;
+
