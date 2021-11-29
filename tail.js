648a4b580355f9a5369a2f1291e0153cebcd952c
fathima-thasnim <thasnimfathima492@gmail.com>
2021-11-29 21:09:37 +0000

    refactor tail

--- tail.js
+++ tail.js
@@ -1,13 +1,13 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+
+
 const tail = function(arr1) {
   arr1.shift();
   console.log(arr1);
 };
+
+
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result, ["Lighthouse", "Labs"]);
+
+module.exports = tail;
