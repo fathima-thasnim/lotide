63e7abf2f1c9e607dba7a8e46217b9c80fb1b108
fathima-thasnim <thasnimfathima492@gmail.com>
2021-11-30 01:03:34 +0000

    updated index.js

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
+const countLetter = require('./countLetters');
+const countOnly = require('./countOnly');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
 
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle
+  head,
+  tail,
+  middle,
+  countLetter,
+  countOnly,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  map,
+  takeUntil,
+  without,
+  
 };
